fa589a5a37914cb0a1b2c7e3e577d8983978a4d4
cbauma05 <cbauma05@uoguelph.ca>
2025-08-10 22:50:47 -0400

    fixed urdf filepath for rviz

--- src/echo_sim_v1/setup.py
+++ src/echo_sim_v1/setup.py
@@ -14,7 +14,7 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
         (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
-        (os.path.join('share', package_name, 'worlds'), glob('worlds/*.sdf')),   # Fixed here
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*.sdf')),  
         (os.path.join('share', package_name, 'models'), glob('models/*.sdf')),
         (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
         (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
